37d025ecb37c0dbda50236dbfc54af58cb801b36
Andrew <ajclink03@gmail.com>
2019-08-05 23:40:28 -0400

    Ooof, Prime Numbers. Starting with the example to make sure my math is right; find the Prime Factors of 13195 (which are graciously provided for us: 5, 7, 13, 29). 	Steps: 			1) Get the first 40 prime numbers and stash them in a vector for use with math 			2) Determine the Prime Factors of said number 			3) Hopefully find the largest Prime Factor 	TO DO: 		getPrimes() 		findFactors()

---
+++ PE 3/Header.h
@@ -0,0 +1,7 @@
+#pragma once
+// header file for all my Project Euler stuff
+#include <string.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <math.h>
---
+++ PE 3/Source.cpp
@@ -0,0 +1,16 @@
+/*
+Andrew Clink
+Project Euler coding practice
+
+I do these for fun and practice, please don't steal, tia.
+*/
+
+#include "Header.h"
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
